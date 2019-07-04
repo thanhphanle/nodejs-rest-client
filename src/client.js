e55b5298fba375d389e0afdb56bcf8b25500e5ef
ThanhPL <phanlethanh93@gmail.com>
2019-07-04 14:56:36 +0700

    Refactor code and add API using params with multiple values

--- src/client.js
+++ src/client.js
@@ -3,13 +3,16 @@ const qs = require('qs');
 
 const RestClient = class {
 
-    // Post login
-    login(username,password) {
+    /**
+     * 
+     * POST login
+     */
+    login(username, password) {
         let data = {
             username: username,
             password: password
         }
-        axios.post('http://localhost:8080/login', data)
+        axios.post('http://localhost:3000/login', data)
             .then(response => {
                 // Handle success.
                 console.log(response.data)
@@ -20,13 +23,16 @@ const RestClient = class {
             });
     }
 
-    // Get list of user data
-    // http://localhost:8080/users
+    /**
+     * 
+     * Get list of user data
+     * Call endpoint: http://localhost:3000/users
+     */
     getUsers(token) {
         let headers = {
             'Authorization': 'Bearer ' + token
         }
-        axios.get('http://localhost:8080/users', { headers: headers, timeout: 5000 })
+        axios.get('http://localhost:3000/users', { headers: headers, timeout: 5000 })
             .then(response => {
                 // Handle success.
                 console.log(response.data)
@@ -37,16 +43,19 @@ const RestClient = class {
             });
     }
 
-    // Get user data by id with Authorization header
-    // http://localhost:8080/users?id=<id>
-    getUser(id) {
+    /**
+     * 
+     * GET user data by id with Authorization header and query param
+     * Call endpoint: http://localhost:3000/users?id=<id>
+     */
+    getUser(token, id) {
         let headers = {
             'Authorization': 'Bearer ' + token
         }
         let params = {
             id: id
         }
-        axios.get('http://localhost:8080/users', { params: params, headers: headers, timeout: 5000 })
+        axios.get('http://localhost:3000/users', { params: params, headers: headers, timeout: 5000 })
             .then(response => {
                 // Handle success.
                 console.log(response.data)
@@ -57,19 +66,46 @@ const RestClient = class {
             });
     }
 
-    // Post with Authorization header and json body
-    saveUser(token,user) {
+    /**
+     * GET user data by multiple name in query param => use URLSearchParams
+     * Call endpoint: http://localhost:3000/users?name=<name1>&name=<name2>&name=<name3>
+     */
+    getUserByNames(token, names) {
+        let headers = {
+            'Authorization': 'Bearer ' + token
+        }
+        let params = URLSearchParams();
+        if (names !== undefined) {
+            for (var i = 0; i < names.length; i++) {
+                params.append('name', names[i]);
+            }
+        }
+        axios.get('http://localhost:3000/users', { params: params, headers: headers, timeout: 5000 })
+            .then(response => {
+                // Handle success.
+                console.log(response.data)
+            })
+            .catch(error => {
+                // Handle error.
+                console.error('An error occurred:', error);
+            });
+    }
+
+    /**
+     * POST with Authorization header and json body 
+     */
+    saveUser(token, user) {
         let headers = {
             'Authorization': 'Bearer ' + token,
             'Content-Type': 'application/json'
         }
         let data = {
-            username: user.username === undefined? '' : user.username,
-            password: user.password === undefined? '' : user.password,
-            fullName: user.fullName === undefined? '' : user.fullName,
-            email: user.email === undefined? '' : user.email
+            username: user.username === undefined ? '' : user.username,
+            password: user.password === undefined ? '' : user.password,
+            fullName: user.fullName === undefined ? '' : user.fullName,
+            email: user.email === undefined ? '' : user.email
         }
-        axios.post('http://localhost:8080/user', data, { headers: headers })
+        axios.post('http://localhost:3000/users', data, { headers: headers })
             .then(response => {
                 // Handle success.
                 console.log(response.data)
@@ -80,8 +116,11 @@ const RestClient = class {
             });
     }
 
-    // Post with content-type = application/x-www-form-urlencoded
-    submitForm(token,name,value) {
+    /**
+     * 
+     * POST with content-type = application/x-www-form-urlencoded
+     */
+    submitForm(token, name, value) {
         let headers = {
             'Authorization': 'Bearer ' + token,
             'Content-Type': 'application/x-www-form-urlencoded'
@@ -90,7 +129,7 @@ const RestClient = class {
             name: name,
             value: value
         }
-        axios.post('http://localhost:8080/submit-form', qs.stringify(data), { headers: headers })
+        axios.post('http://localhost:3000/submit-form', qs.stringify(data), { headers: headers })
             .then(response => {
                 // Handle success.
                 console.log(response.data)
